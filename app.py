572a118c43b4c1092b33924c12d1bbcd756b4a14
Aarni Honka <aarni.honka@gmail.com>
2022-10-01 06:20:39 +0300

    Add support to define quote amount with env variable.

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import json
 import os
 from time import sleep
-from typing import Optional
+from typing import Any, Tuple
 
 import requests
 
 URL = "http://api.forismatic.com/api/1.0/"
 
 
-def get_data() -> Optional[dict]:
+def get_data() -> Tuple[Any, Any]:
     response = requests.post(
         URL, data={"method": "getQuote", "format": "json", "lang": "en"}
     )
 
     if response.status_code == 200:
-        return response.json()
+        data = response.json()
+        return data.get("quoteText"), data.get("quoteAuthor")
 
     else:
         print(f"Failed to get data from API, error: {response.status_code}")
-        return False
+        return None, None
 
 
 def main():
-    data = get_data()
-    print(f"\n{data.get('quoteText')} - {data.get('quoteAuthor')}\n")
+    number_of_quotes = int(os.getenv("QUOTES", 1))
+    quote, author = get_data()
+
+    # Limit maximum to 5 to prevent API overload.
+    if number_of_quotes > 5:
+        number_of_quotes = 5
+
+    for _ in range(number_of_quotes):
+        quote, author = get_data()
+
+        if quote and author:
+            if number_of_quotes > 1:
+                sleep(1)
+            print(f"\n{quote} - {author}\n")
 
 
 if __name__ == "__main__":
